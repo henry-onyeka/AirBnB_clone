4d25bea2ea9443c938523eae4eecb0436dff258a
henry-onyeka <hmike54@gmail.com>
2023-02-18 23:11:39 +0000

    corrected

--- models/base_model.py
+++ models/base_model.py
@@ -16,6 +16,25 @@ class BaseModel:
         self.created_at = dt.now()
         self.updated_at = self.created_at
 
+        if kwargs:
+            form = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                    continue
+                if key == "created_at":
+                    self.created_at = dt.strptime(value, form)
+                    continue
+                if key == "updated_at":
+                    self.updated_at = dt.strptime(value, form)
+                    continue
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = dt.now()
+            models.storage.new(self)
+
     def __str__(self):
         '''string rep'''
         return '[{}] ({}) {}'.format(
