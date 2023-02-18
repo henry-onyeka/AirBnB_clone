de73ad59875ee95d3d48f339439bd632d7683420
henry-onyeka <hmike54@gmail.com>
2023-02-18 22:22:50 +0000

    corrected

--- models/base_model.py
+++ models/base_model.py
@@ -4,16 +4,36 @@ class to define all class and models
 '''
 from uuid import uuid4
 from datetime import datetime as dt
+import models
 
 
 class BaseModel:
     '''method definition for other classes'''
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''initialisation'''
         self.id = str(uuid4())
         self.created_at = dt.now()
         self.updated_at = self.created_at
+
+        if kwargs:
+            form = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                    continue
+                if key == "created_at":
+                    self.created_at = dt.strptime(value, form)
+                    continue
+                if key == "updated_at":
+                    self.updated_at = dt.strptime(value, form)
+                    continue
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = dt.now()
+            models.storage.new(self)
 
     def __str__(self):
         '''string rep'''
