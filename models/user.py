a011f204b7fafc77e7680589a563a340371ffebd
6felix <felix.okoronkwo.1@gmail.com>
2023-02-19 19:11:28 +0000

    fixed user class

--- models/user.py
+++ models/user.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""User Class"""
 
 from models.base_model import BaseModel
 
-class User(BaseModel):
 
+class User(BaseModel):
+    """Class"""
     email = ""
     password = ""
     first_name = ""
