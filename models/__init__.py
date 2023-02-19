be4f5808967fd88affdf1d6aea0dfa42d771a043
6felix <felix.okoronkwo.1@gmail.com>
2023-02-19 21:23:05 +0000

    console

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python33
 
 from models.engine.file_storage import FileStorage
 storage = FileStorage()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """FileStorage Class"""
 import json
 from models.base_model import BaseModel
