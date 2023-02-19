550005b62396a2ffa8d1a4d765aaca9b2d6f7a10
6felix <felix.okoronkwo.1@gmail.com>
2023-02-19 19:09:09 +0000

    fixed review

--- models/review.py
+++ models/review.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+"""Review Class"""
+
 from models.base_model import BaseModel
 
+
 class Review(BaseModel):
+    """Class"""
     place_id = ""
     user_id = ""
     text = ""
